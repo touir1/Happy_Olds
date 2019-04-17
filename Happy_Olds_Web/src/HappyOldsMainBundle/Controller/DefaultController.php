595f456eceabdee09073c6dbe8ed61001b09a921
touir1 <touir.mat@gmail.com>
2019-04-17 20:24:35 +0100

    Template integration

--- Happy_Olds_Web/src/HappyOldsMainBundle/Controller/DefaultController.php
+++ Happy_Olds_Web/src/HappyOldsMainBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('HappyOldsMainBundle:Default:index.html.twig');
+        return $this->render('@HappyOldsMain/Default/index.html.twig');
     }
 }
