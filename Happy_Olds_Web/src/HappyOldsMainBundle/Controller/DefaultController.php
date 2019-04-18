2f817b2d1f4277f86125d52c235c7ff9c485b1b3
MedAmineSadfi <mohamedamine.sadfi@esprit.tn>
2019-04-18 23:28:47 +0200

    adding page login /update page home

--- Happy_Olds_Web/src/HappyOldsMainBundle/Controller/DefaultController.php
+++ Happy_Olds_Web/src/HappyOldsMainBundle/Controller/DefaultController.php
@@ -8,6 +8,10 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        return $this->render('@HappyOldsMain/Default/login.html.twig');
+    }
+    public function accueilAction()
+    {
         return $this->render('@HappyOldsMain/Default/index.html.twig');
     }
 }
